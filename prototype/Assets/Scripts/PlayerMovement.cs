cbd40e400bed9134760539afc994ee8823832999
Ryan Lawrence <Volpanicstudios@gmail.com>
2020-10-08 11:23:57 +1100

    Imported some basic first person control scripts.
    Imported MouseLook and PlayerMovement and set them up in the scene.

---
+++ prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    bool isGrounded = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position,groundDistance,groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xMov + transform.forward * zMov;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
