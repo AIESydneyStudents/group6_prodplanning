f18cdf51afadeddbb212d2a3cc948edbc02c6520
Volpanic <volpanicstudios@gmail.com>
2020-10-10 17:51:09 +1100

    Portals now render semi properly, not really in a usable state yet.

---
+++ prototype/Assets/PortalCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform PlayerCamera;
+
+    [Tooltip("The portal the camera is linked to.")]
+    public Transform portal;
+
+    [Tooltip("The source portal.")]
+    public Transform otherPortal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = PlayerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angleDifferenceBetweenPortalsRots = Quaternion.Angle(portal.rotation,otherPortal.rotation);
+        Quaternion portalRotationalDif = Quaternion.AngleAxis(angleDifferenceBetweenPortalsRots,Vector3.up);
+        Vector3 newCamDir = portalRotationalDif * PlayerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCamDir,Vector3.up);
+
+    }
+}
---
+++ prototype/Assets/PortalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public Camera cameraBelow;
+    public Material cameraMatB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cameraBelow.targetTexture != null)
+        {
+            cameraBelow.targetTexture.Release();
+        }
+        cameraBelow.targetTexture = new RenderTexture(Screen.width,Screen.height,24);
+        cameraMatB.mainTexture = cameraBelow.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
