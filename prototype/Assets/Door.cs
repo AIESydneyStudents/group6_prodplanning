48e55800b1c258dead2bbacd5e42efa2801665ab
Ryan Lawrence <Volpanicstudios@gmail.com>
2020-10-22 16:06:50 +1100

    Feature (Gameplay): Added a basic door opening script.

---
+++ prototype/Assets/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : MonoBehaviour
+{
+    bool Open = false;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ToggleDoor()
+    {
+        if(Open)
+        {
+            animator.Play("ani_door_close");
+        }
+        else
+        {
+            animator.Play("ani_door_open");
+        }
+
+        Open = !Open;
+    }
+}
