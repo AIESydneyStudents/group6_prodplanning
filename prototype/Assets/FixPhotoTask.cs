2593d431732cb98d834e12869dec83079bd805b3
Ryan Lawrence <Volpanicstudios@gmail.com>
2020-11-04 16:40:34 +1100

    Feature (Gameplay): WIP aligning photo frame task.

---
+++ prototype/Assets/FixPhotoTask.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPhotoTask : Task
+{
+    F_PlayerMovement player;
+    public CinemachineVirtualCamera TaskCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taskRunning = true;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<F_PlayerMovement>();
+    }
+
+    Vector2 previousMosuePos = Vector2.zero;
+
+    private void Update()
+    {
+        if(taskRunning)
+        {
+            float dir = Input.mousePosition.x - previousMosuePos.x;
+
+            transform.rotation *= Quaternion.Euler(dir * 0.05f,0,0);
+
+            previousMosuePos = Input.mousePosition;
+        }
+    }
+
+    public void InteractWithPhoto()
+    {
+        if(!taskFinished)
+        {
+            player.ChangePerspective(TaskCamera);
+            taskRunning = true;
+
+            previousMosuePos = Input.mousePosition;
+        }
+    }
+}
