350ee1d4577e1ea8d1f084170dd57141c8da7b8a
Ryan Lawrence <Volpanicstudios@gmail.com>
2020-11-04 11:05:25 +1100

    Feature (Gameplay): Added various ways to trigger dialouge.
    Added new font, Made dialogue look prettier with a fade in and out.

--- prototype/Assets/DialougeManager.cs
+++ prototype/Assets/DialougeManager.cs
@@ -8,8 +8,6 @@ public class DialougeManager : MonoBehaviour
     public TextMeshProUGUI Text;
     public AudioSource AudioPlayer;
 
-    public DialougeSequence testSequence;
-
     private DialougeSequence playingSequence;
     private int dialougeIndex = 0;
 
@@ -25,11 +23,6 @@ public class DialougeManager : MonoBehaviour
         Text.enabled = true;
     }
 
-    private void Start()
-    {
-        PlayDialouge(testSequence);
-    }
-
     private float lineTimer = 0;
     private float lineMaxTimer = 0;
     private float charIncereaseRate = 0;
@@ -43,6 +36,7 @@ public class DialougeManager : MonoBehaviour
                 //Set dialouge text
                 Text.text = playingSequence.textStrings[dialougeIndex].DialougeText;
 
+                Text.alpha = 0;
                 Text.maxVisibleCharacters = 0;
                 lineTimer = 0;
 
@@ -63,6 +57,15 @@ public class DialougeManager : MonoBehaviour
                 lineTimer += Time.unscaledDeltaTime; // Unscaled because we don't want long seconds.
                 Text.maxVisibleCharacters = Mathf.CeilToInt(((lineTimer) / (lineMaxTimer * 0.5f)) * Text.textInfo.characterCount);
 
+                //Fade text if started or done
+                float perc = (lineTimer) / (lineMaxTimer);
+                FadeText(perc);
+
+                if(perc > 0.95f)
+                {
+                    Text.alpha = 1 - (perc - 0.95f) * 20;
+                }
+
                 if (lineTimer >= lineMaxTimer) // Lined finish speaking
                 {
                     //Go to next line
@@ -77,5 +80,19 @@ public class DialougeManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void FadeText(float percentDone)
+    {
+        if (percentDone >= 0.95f)
+        {
+            Text.alpha = 1 - (percentDone - 0.95f) * 20;
+        }
+        else if (percentDone <= 0.05f)
+        {
+            Text.alpha = (percentDone) * 20;
+        }
+
+        Text.alpha = Mathf.Clamp(Text.alpha,0f,1f);
     }
 }
---
+++ prototype/Assets/PlayDialougeOnCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayDialougeOnCollision : MonoBehaviour
+{
+    public bool OnlyOnce = true;
+    public DialougeSequence DialougeToPlay;
+
+    private bool hasPlayed = false;
+    private DialougeManager dialougeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialougeManager = GameObject.FindGameObjectWithTag("DialougeSystem").GetComponent<DialougeManager>();
+    }
+
+    private void PlayDialouge()
+    {
+        if (!hasPlayed)
+        {
+            dialougeManager.PlayDialouge(DialougeToPlay);
+            if (OnlyOnce) 
+            {
+                hasPlayed = true;
+                Destroy(gameObject);
+            };
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayDialouge();
+        }
+    }
+}
