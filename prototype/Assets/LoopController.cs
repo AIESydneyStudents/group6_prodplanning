7b46a5cb85446b0747af2355d8c137fba3e2b589
Ryan Lawrence <Volpanicstudios@gmail.com>
2020-11-19 11:44:03 +1100

    Update (Gameplay): Added saturation slider to each loop.
    Made saturation transition nicely as well.

---
+++ prototype/Assets/LoopController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LoopController : MonoBehaviour
+{
+    [Range(0f,1f)]
+    public float SaturationLevel = 1.0f;
+    public UnityEvent OnLoopStart;
+
+}
